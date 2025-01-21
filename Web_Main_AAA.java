5e453fafc17fd830b78c846e940e048e7cfbcf60
smwmr2001a <smwmr2001a@gmail.com>
2025-01-21 13:46:22 +0900

    Add files via upload

---
+++ Web_Main_AAA.java
@@ -0,0 +1,25 @@
+
+
+
+// main class
+
+public class Web_Main_AAA {
+    public static void main(String[] args) {
+        System.out.println("Web Application AAA Started");
+        System.out.println("Web Application AAA Started");
+        System.out.println("Web Application AAA Started");
+        System.out.println("Web Application AAA Started");
+
+        System.out.println("Web Application AAA Started");
+        System.out.println("Web Application AAA Started");
+        System.out.println("Web Application AAA Started");
+        System.out.println("Web Application AAA Started250121-115817#");
+        System.out.println("Web Application AAA Started250121-115817#");
+
+
+        /// comment 250121-120818#
+        // coomment
+        // comment 250121-120509#
+        
+    }
+}
